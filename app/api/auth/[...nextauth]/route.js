9347c9d1222bd7e1a1817b4630bd1a1a035a6dbf
Dalia Abbruciati <daliaabbruciati@gmail.com>
2024-02-13 19:37:53 +0100

    google provider auth set-up

---
+++ app/api/auth/[...nextauth]/route.js
@@ -0,0 +1,54 @@
+import NextAuth from "next-auth/next";
+import GoogleProvider from "next-auth/providers/google";
+
+import User from "@models/user";
+import { connectToDB } from "@utils/database";
+
+/* Go on 'console.cloud.google.com' and create a new project
+and then get the credentials */
+
+const handler = NextAuth({
+    providers: [
+        GoogleProvider({
+            clientId: process.env.GOOGLE_ID,
+            clientSecret: process.env.GOOGLE_SECRET
+        })
+    ],
+    async session({ session }) {
+			// get current user session
+			const sessionUser = await User.findOne({
+					email: session.user.email
+			})
+
+			// update the user ID to know which user is currently online
+			session.user.id = sessionUser._id.toString()
+			return session;
+    },
+
+    async signIn({ profile }) {
+			try{
+					await connectToDB();
+
+					// check if user already exist
+					const userExists = await User.findOne({
+							email: profile.email
+					})
+
+					// if not, create a new user
+					if(!userExists) {
+							await User.create({
+									email: profile.email,
+									username: profile.name.replace(" ", "").toLowerCase(),
+									image: profile.picture
+							})
+					}
+					return true
+
+        }catch(error){
+            console.log(error)
+            return false
+        }
+    }
+})
+
+export { handler as GET, handler as POST }
--- components/Nav.jsx
+++ components/Nav.jsx
@@ -8,17 +8,17 @@ import { useState, useEffect } from "react"
 import { signIn, signOut, useSession, getProviders } from "next-auth/react"
 
 const Nav = () => {
-  const isUserLoggedIn = true
+  const { data: session } = useSession()
   /* Providers is an object with all the providers related to Auth, for example Google, Github, etc. */
   const [providers, setProviders] = useState(null)
   const [toggleDropdown, setToggleDropdown] = useState(false)
 
   useEffect(() => {
-    const setProviders = async () => {
+    const setUpProviders = async () => {
       const response = await getProviders()
       setProviders(response)
     }
-    setProviders()
+    setUpProviders()
 
   }, [])
 
@@ -31,12 +31,12 @@ const Nav = () => {
 
       {/* Desktop Navigation */}
       <div className="sm:flex hidden">
-        {isUserLoggedIn ? (
+        {session?.user ? (
           <div className="flex gap-3 md:gap-5">
             <Link className="black_btn" href="/create-prompt">Create post</Link>
             <button type="button" onClick={signOut} className="outline_btn">Sign Out</button>
             <Link href={"/profile"}>
-              <Image src="/assets/images/logo.svg" width={37} height={37} className="rounded-full" alt="profile"/>
+              <Image src={session?.user.image} width={37} height={37} className="rounded-full" alt="profile"/>
             </Link>
           </div>
         ) : (
@@ -56,12 +56,12 @@ const Nav = () => {
         )}
       </div>
 
-      {/* Desktop Navigation */}
+      {/* Mobile Navigation */}
       <div className="sm:hidden flex relative">
-        {isUserLoggedIn ? (
+        {session?.user ? (
           <div className="flex">
             <Image
-            src="/assets/images/logo.svg"
+            src={session?.user.image}
             width={37}
             height={37}
             className="rounded-full"
