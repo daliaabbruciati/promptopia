9b37971fc4518087d3e4d64f299b37cb164cb62a
Dalia Abbruciati <daliaabbruciati@gmail.com>
2024-02-14 14:43:39 +0100

    add Feed page to get user prompts

--- components/Feed.jsx
+++ components/Feed.jsx
@@ -1,8 +1,61 @@
-import React from 'react'
+"use client"
+
+import { useEffect, useState } from "react"
+import PromptCard from "./PromptCard"
+
+const PromptCardList = ({data, handleTagClick}) => {
+  return (
+    <div className="mt-16 prompt_layout">
+      {data.map((post) => (
+        <PromptCard
+          key={post._id}
+          post={post}
+          handleTagClick={handleTagClick}
+        />
+      ))}
+    </div>
+  )
+}
 
 const Feed = () => {
+  const [searchText, setSearchText] = useState("")
+  const [posts, setPost] = useState([])
+
+  const handleSearchChange = (e) => {
+    setSearchText(e.target.value)
+  }
+
+  /** I need to call the API to GET the posts list */
+  useEffect(() => {
+    const fetchPost = async () => {
+      const response = await fetch('/api/prompt')
+      const data = await response.json()
+
+      setPost(data)
+    }
+    fetchPost()
+  }, [])
+
   return (
-    <div>Feed</div>
+    <section className="feed">
+      <form className="relative w-full flex-center">
+        <input 
+          type="text"
+          placeholder="search for a tag or username"
+          value={searchText}
+          onChange={handleSearchChange}
+          required
+          className="search_input peer"
+        />
+      </form>
+
+    <PromptCardList
+      data={posts}
+      handleTagClick={() => {}}
+      searchText={searchText}
+    />
+
+    </section>
   )
 }
 
